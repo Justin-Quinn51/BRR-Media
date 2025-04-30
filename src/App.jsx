bc82142f2b58fc2b813ce6b1e6e6d1030e18c9c7
Justin Quinn <jpq3366@gmail.com>
2025-04-30 15:40:49 +0100

    Add main files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import "./index.css";
+
+function App() {
+  return (
+    <main className="bg-blue-700">
+      <Header />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center my-2 mx-5">
+      <img src="../../public/BRR-Logo.png" alt="Logo" />
+      <i class="fa-regular fa-circle-user text-3xl"></i>
+    </div>
+  );
+}

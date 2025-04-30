12f49549520fce6469959d1c0fddf9dbdfd97e0f
Justin Quinn <jpq3366@gmail.com>
2025-04-30 15:42:58 +0100

    Fixed relative path from public folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="flex justify-between items-center my-2 mx-5">
-      <img src="../../public/BRR-Logo.png" alt="Logo" />
+      <img src="../../BRR-Logo.png" alt="Logo" />
       <i class="fa-regular fa-circle-user text-3xl"></i>
     </div>
   );
